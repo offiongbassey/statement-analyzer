d7835f4d203a951e74636bc6ca5212f2f3fbcd3c
offiongbassey <naijaspeed1@gmail.com>
2023-09-22 15:50:26 +0100

    View Users Completed

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -12,3 +12,31 @@ export const createUser = async(req, res) => {
         return responseHandler(res, 500, false, "An error occured, please try again later");
     }
 }
+
+export const viewUsers = async(req, res) => {
+    try {
+        const users = await Model.User.findAll();
+        if(users.length > 0){
+        return responseHandler(res, 200, true, "Users found", users);
+        }
+        return responseHandler(res, 404, false, "No User Available");
+
+    } catch (error) {
+        await errorHandler(error);
+        return responseHandler(res, 500, false, "An error occured, please try again later");
+    }
+}
+
+export const viewUser = async(req, res) => {
+    try {
+        const user = await Model.User.findOne({ where: { id: req.params.user_id } });
+        if(user){
+            return responseHandler(res, 200, true, "User found", user);
+        }
+            return responseHandler(res, 404, false, "User not found");
+            
+    } catch (error) {
+        await errorHandler(error);
+        return responseHandler(res, 500, false, "An error occured, please try again later");
+    }
+}
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { createUser } from "../controllers/user";
+import { createUser, viewUser, viewUsers } from "../controllers/user";
 import { validationHandler } from "../helpers/validation";
 import { user_validator } from "../middlewares/validator";
 
 const router = express.Router();
 
 router.post('/create-user', validationHandler(user_validator), createUser);
+router.get('/user/:user_id', viewUser);
+router.get('/users', viewUsers);
+
 
 module.exports = router;
